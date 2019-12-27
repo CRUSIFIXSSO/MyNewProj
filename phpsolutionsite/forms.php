43e224cb69b546dc04377a9954552213ce1f2589
Atuan O <crusifixsso@gmail.com>
2019-12-27 17:04:58 +0000

    Add files via upload

---
+++ phpsolutionsite/forms.php
@@ -0,0 +1,29 @@
+
+     <!DOCTYPE html>
+     <html lang="en">
+     <head>
+             <meta charset="UTF-8">
+             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+             <meta http-equiv="X-UA-Compatible" content="ie=edge">
+             <title>Document</title>
+     </head>
+     <body>
+             <form method="post" action="">
+             <p>
+                <label for="name">Name:</label>
+                <input name="name" id="name" type="text">
+             </p>
+             <p>
+                 <label for="email">Email:</Label>
+                 <input name="email" id="email" type="text">
+             </p>
+             <p>
+                <label for="comments">Comments:</label>
+                <textarea name="comments" id="comments"></textarea>
+             </p>
+             <p>
+                 <input name="send" type="submit" value="Send messgae">
+             </p>
+             </form>
+     </body>
+     </html>
